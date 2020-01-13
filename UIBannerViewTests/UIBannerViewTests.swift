a4a502963fbad5660bbed051dc4e72796df2043d
Volkan Sönmez <volkan.sonmez@etts.com.tr>
2020-01-13 13:41:25 +0300

    Initial Commit

---
+++ UIBannerViewTests/UIBannerViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  UIBannerViewTests.swift
+//  UIBannerViewTests
+//
+//  Created by Volkan Sonmez on 13.01.2020.
+//  Copyright © 2020 Porte. All rights reserved.
+//
+
+import XCTest
+@testable import UIBannerView
+
+class UIBannerViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
