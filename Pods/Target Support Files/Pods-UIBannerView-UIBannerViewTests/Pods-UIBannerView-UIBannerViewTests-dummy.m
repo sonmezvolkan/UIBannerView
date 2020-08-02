bdbbc031bd8984b9aa177b35dff1db6b2e785f63
Volkan Sonmez <sonmez.volkan92@gmail.com>
2020-08-02 20:54:43 +0300

    Release 1.0.2

---
+++ Pods/Target Support Files/Pods-UIBannerView-UIBannerViewTests/Pods-UIBannerView-UIBannerViewTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIBannerView_UIBannerViewTests : NSObject
+@end
+@implementation PodsDummy_Pods_UIBannerView_UIBannerViewTests
+@end
---
+++ Pods/Target Support Files/Pods-UIBannerView/Pods-UIBannerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIBannerView : NSObject
+@end
+@implementation PodsDummy_Pods_UIBannerView
+@end
